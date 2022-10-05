75321b4e19e23a5af146271c0582a8b1c03ca68d
Tim Armstrong <tim@plaintextnerds.com>
2022-10-05 17:11:58 +0100

    examples

--- app/pydantic_models.py
+++ app/pydantic_models.py
@@ -28,14 +28,62 @@ class DriverInputs(BaseModel):
     Resource: Dict[str, object] = Field(alias='resource')
     Driver: Optional[ValuesSecrets] = Field(alias='driver')
 
-
-class DriverOutputs(BaseModel):
-    GUResID: str = Field(alias='id')
-    Type: str = Field(alias='type')
-    Resource: ValuesSecrets = Field(alias='resource', default=ValuesSecrets())
-    Manifests: List["Manifest"] = Field(alias='manifests', default=[])
+    class Config:
+        schema_extra = {
+            "example": {
+                "type": "s3",
+                "resource": {},
+                "driver": {
+                    "values": {
+                        "region": "eu-west-1"
+                    },
+                    "secrets": {
+                        "account": {
+                            "aws_access_key_id": "",
+                            "aws_secret_access_key": ""
+                        }
+                    }
+                }
+            }
+        }
 
 
 class Manifest(BaseModel):
     Location: str = Field(alias='location')
     Data: object = Field(alias='data')
+
+
+class DriverOutputs(BaseModel):
+    GUResID: str = Field(alias='id')
+    Type: str = Field(alias='type')
+    Resource: ValuesSecrets = Field(alias='resource', default=ValuesSecrets())
+    Manifests: List[Manifest] = Field(alias='manifests', default=[])
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "id": "test-2",
+                "type": "s3",
+                "resource": {
+                    "values": {
+                        "region": "eu-west-1",
+                        "bucket": "dd55f0e8-4e42-4c7c-a65c-0b6a70492d14"
+                    }
+                }
+            }
+        }
+
+
+class Health(BaseModel):
+    App: str = Field(alias="app")
+    Version: str = Field(alias="version")
+    Status: str = Field(alias="status")
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "app": "reference-driver",
+                "version": "0.0.0",
+                "status": "OK"
+            }
+        }
